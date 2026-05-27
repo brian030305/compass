473b5458872979005fca6345911cc541bada08c4
brian030305 <brian030305@gmail.com>
2026-05-27 14:54:41 +0900

    수정

--- data_manager.py
+++ data_manager.py
@@ -208,7 +208,7 @@ def fetch_bizinfo_api():
     try:
         engine = get_oracle_engine()
         # 🚨 구글 시트 대신 오라클 DB의 'bizinfo_tb' 방에서 데이터를 즉시 가져옵니다.
-        df = pd.read_sql("SELECT * FROM bizinfo_tb", engine)
+        df = pd.read_sql("SELECT * FROM bizinfo_tb", engine.execution_options(timeout=10))
         
         if df.empty:
             return pd.DataFrame()
